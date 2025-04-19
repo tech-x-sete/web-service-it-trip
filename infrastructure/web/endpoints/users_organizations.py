ef749a2e3b6d21e7a55fbbb88e46665406ed3416
Ugmar <marchukovanton6@gmail.com>
2025-04-19 19:54:35 +0300

    users_organizations.py

---
+++ infrastructure/web/endpoints/users_organizations.py
@@ -0,0 +1,36 @@
+from ..main import app
+
+
+@app.route('/v1/users/{username}', methods=['GET'])
+def get_users():
+    ...
+
+
+@app.route('/v1/organization/{organization_id}', methods=['GET'])
+def get_organizations():
+    ...
+
+
+@app.route('/v1/writers', methods=['GET'])
+def get_writers():
+    ...
+
+
+@app.route('/v1/writers', methods=['POST'])
+def post_writers():
+    ...
+
+
+@app.route('/v1/writers/{ user_id }', methods=['DELETE'])
+def delete_writers():
+    ...
+
+
+@app.route('/v1/organizations', methods=['POST'])
+def post_organizations():
+    ...
+
+
+@app.route('/v1/organizations/{ organization_id }', methods=['DELETE'])
+def delete_organizations():
+    ...
