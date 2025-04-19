b47cbc1f9fd72f1dc423c661a171fa8211f524db
SBmstu <sbe.daily@bmstu.ru>
2025-04-19 08:56:21 +0300

    some changes

--- telegram_bot/handlers.py
+++ telegram_bot/handlers.py
@@ -1,5 +1,5 @@
 from aiogram import Router, F
-from aiogram.filters import CommandStart, Command
+from aiogram.filters import Command
 from aiogram.types import Message, CallbackQuery
 
 router = Router()
