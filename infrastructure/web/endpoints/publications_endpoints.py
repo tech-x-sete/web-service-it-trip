d1e58fa36a0e414048a670a8faeaa4f952ce37ee
bia24u810 <bia24u810@student.bmstu.ru>
2025-04-19 21:44:27 +0300

    first blueprint

--- infrastructure/web/endpoints/publications_endpoints.py
+++ infrastructure/web/endpoints/publications_endpoints.py
@@ -1,32 +1,36 @@
-from ..init import app
 from infrastructure.db.models import async_session
 from infrastructure.db.repositories.publication_repository import PublicationRepository
-from flask import render_template
+from flask import render_template, Blueprint
+
+publications_bp = Blueprint('publications', __name__)
 
 
-@app.route('/v1/publications', methods=['GET'])
-def get_publications():
+@publications_bp.route('/', methods=['GET'])
+async def get_publications():
     async with async_session() as session:
         repo = PublicationRepository(session)
-        data = repo.get_all_publications()
-        render_template('templates/', data=data)
+        data = await repo.get_all_publications()
+        if data:
+            return "Test"
+        # render_template('templates/', data=data)
+        return data
 
 
-@app.route('/v1/publications/{publication_id}', methods=['GET'])
+@publications_bp.route('/{publication_id}', methods=['GET'])
 def get_publication_by_id(id_: int):
     ...
 
 
-@app.route('/v1/publications', methods=['POST'])
+@publications_bp.route('/', methods=['POST'])
 def add_publication():
     ...
 
 
-@app.route('/v1/publications/{publication_id}', methods=['PATCH'])
+@publications_bp.route('/{publication_id}', methods=['PATCH'])
 def edit_publication(id_: int):
     ...
 
 
-@app.route('/v1/publications/{publication_id}', methods=['DELETE'])
+@publications_bp.route('/{publication_id}', methods=['DELETE'])
 def delete_publication(id_: int):
     ...
--- infrastructure/web/init.py
+++ infrastructure/web/init.py
@@ -1,15 +1,8 @@
 from flask import Flask, render_template
-
-import functions
+from endpoints.publications_endpoints import publications_bp
 
 app = Flask(__name__)
-
-
-@app.route('/', methods=['GET'])
-@app.route('/main', methods=['GET'])
-def newsfeed():
-    news = functions.load_news()
-    return render_template('newsfeed.html', news=news)
+app.register_blueprint(publications_bp, url_prefix='/publications')
 
 
 if __name__ == '__main__':
