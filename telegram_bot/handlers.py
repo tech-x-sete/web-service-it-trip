77501f338ec908e9872cac5b889c2baa5e942524
SBmstu <sbe.daily@bmstu.ru>
2025-04-19 04:16:41 +0300

    get_list_keyboard

--- telegram_bot/handlers.py
+++ telegram_bot/handlers.py
@@ -1,15 +1,21 @@
 from aiogram import Router, F
-from aiogram.filters import CommandStart
+from aiogram.filters import CommandStart, Command
+from aiogram.filters.callback_data import CallbackData
 from aiogram.types import Message
 
 router = Router()
 
 
-@router.message(CommandStart())
+@router.message(Command("start", "get_list"))
 async def start(message: Message):
-    await message.answer("start")
+    await message.answer("Получить список мероприятий", reply_markup=...)
 
 
 @router.message(F.text)
 async def echo(message: Message):
     await message.answer("Echo")
+
+
+@router.callback_query(...)
+async def get_events(callback: CallbackData):
+    await ...
