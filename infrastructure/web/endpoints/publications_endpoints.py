34ed5f37f3ed272abc48c80bd5dd4d6a1a6f2244
SBmstu <sbe.daily@bmstu.ru>
2025-04-20 03:32:40 +0300

    edit_publication

--- infrastructure/web/endpoints/publications_endpoints.py
+++ infrastructure/web/endpoints/publications_endpoints.py
@@ -1,6 +1,6 @@
 from infrastructure.db.models import async_session, Publication
 from infrastructure.db.repositories.publication_repository import PublicationRepository
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
 from core import domain
 from sqlalchemy import select, delete, update
 
@@ -45,9 +45,33 @@ def add_publication(pub: domain.Publication) -> None:
 
 
 @publications_bp.route('/{publication_id}', methods=['PATCH'])
-def edit_publication(id_: int):
+async def edit_publication(publication_id: int):
     async with async_session() as session:
-        ...
+        data = request.get_json()
+        if not data:
+            return {"error": "No data provided"}, 400
+
+        repo = PublicationRepository(session)
+        publication = await repo.get_publication_by_id(publication_id)
+        if not publication:
+            return {"error": "Publication not found"}, 404
+
+        update_data = {
+            "title": data.get("title", publication.title),
+            "content": data.get("content", publication.content),
+            "featured_image_url": data.get("featured_image_url", publication.featured_image_url),
+            "writer_id": data.get("writer_id", publication.writer_id),
+            "organization_id": data.get("organization_id", publication.organization),
+            "publish_date": data.get("publish_date", publication.publish_date),
+            "event_date": data.get("event_date", publication.event_date),
+            "is_archived": data.get("is_archived", publication.is_archived),
+            "location": data.get("location", publication.location)
+        }
+
+        await repo.update_publication(publication_id, **update_data)
+
+        # updated_pub = await repo.get_publication_by_id(publication_id)
+        # return updated_pub
 
 
 @publications_bp.route('/{publication_id}', methods=['DELETE'])
@@ -58,6 +82,9 @@ def delete_publication(publication_id: int):
         )
 
         publication = result.scalars().first()
+        if not publication:
+            raise Exception
+
         session.execute(
             delete(Publication)
             .where(Publication.id == publication_id)
